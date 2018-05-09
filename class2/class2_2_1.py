1e0c8addd2e2d1c25f4375182caa15baa6958622
JerryShang <424431687@qq.com>
2018-05-09 09:32:03 +0800

    logistics分类

--- class2/class2_2_1.py
+++ class2/class2_2_1.py
@@ -16,11 +16,15 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
+
+
 def calc_accuracy(logist, y):
     accuracy = tf.equal(tf.argmax(logist, 1), tf.argmax(y, 1))
     accuracy = tf.cast(accuracy, tf.float32)
     accuracy = tf.reduce_mean(accuracy)
     return sess.run(accuracy, feed_dict={x: X_test, y: Y_test})
+
+
 ##############基本设置########################
 mu1x = 6
 mu2x = 3
@@ -32,18 +36,18 @@ step = 0.01
 batch_train = 20
 len_train = 100
 #训练数据
-Ax = sigma*np.random.randn(len_train, 1) + mu1x
-Ay = sigma*np.random.randn(len_train, 1) + mu1y
-Bx = sigma*np.random.randn(len_train, 1) + mu2x
-By = sigma*np.random.randn(len_train, 1) + mu2y
+Ax = sigma * np.random.randn(len_train, 1) + mu1x
+Ay = sigma * np.random.randn(len_train, 1) + mu1y
+Bx = sigma * np.random.randn(len_train, 1) + mu2x
+By = sigma * np.random.randn(len_train, 1) + mu2y
 
 # plt.plot(Ax,Ay,'r+')
 # plt.plot(Bx,By,'bo')
 # plt.show()
 
-XA = np.append(Ax,Ay, axis=1)
-XB = np.append(Bx,By, axis=1)
-X = np.append(XA,XB, axis=0)
+XA = np.append(Ax, Ay, axis=1)
+XB = np.append(Bx, By, axis=1)
+X = np.append(XA, XB, axis=0)
 # for i in range(100):
 #     print(X[i,:])
 Y0 = np.zeros(shape=(len_train, 1))
@@ -58,91 +62,79 @@ Y = np.append(Y10, Y01, axis=0)
 batch_test = 30
 len_test = 30
 #测试数据
-Ax_test = sigma*np.random.randn(len_test, 1) + mu1x
-Ay_test = sigma*np.random.randn(len_test, 1) + mu1y
-Bx_test = sigma*np.random.randn(len_test, 1) + mu2x
-By_test = sigma*np.random.randn(len_test, 1) + mu2y
+Ax_test = sigma * np.random.randn(len_test, 1) + mu1x
+Ay_test = sigma * np.random.randn(len_test, 1) + mu1y
+Bx_test = sigma * np.random.randn(len_test, 1) + mu2x
+By_test = sigma * np.random.randn(len_test, 1) + mu2y
 
-XA_test = np.append(Ax_test,Ay_test, axis=1)
-XB_test = np.append(Bx_test,By_test, axis=1)
-X_test = np.append(XA_test,XB_test, axis=0)
+XA_test = np.append(Ax_test, Ay_test, axis=1)
+XB_test = np.append(Bx_test, By_test, axis=1)
+X_test = np.append(XA_test, XB_test, axis=0)
 
 Y0_test = np.zeros(shape=(len_test, 1))
-Y01_test = np.append(Y0_test,np.ones(shape=(len_test, 1)), axis=1)
+Y01_test = np.append(Y0_test, np.ones(shape=(len_test, 1)), axis=1)
 Y1_test = np.ones(shape=(len_test, 1))
-Y10_test = np.append(Y1_test,np.zeros(shape=(len_test, 1)), axis=1)
-Y_test = np.append(Y10_test,Y01_test, axis=0)
+Y10_test = np.append(Y1_test, np.zeros(shape=(len_test, 1)), axis=1)
+Y_test = np.append(Y10_test, Y01_test, axis=0)
 
 ####构图########################################
-W = tf.Variable(initial_value=tf.zeros([2,2]), name='Weigh', dtype=tf.float32)
+W = tf.Variable(initial_value=tf.zeros([2, 2]), name='Weigh', dtype=tf.float32)
 b = tf.Variable(initial_value=tf.zeros([2]), name='bias', dtype=tf.float32)
-x = tf.placeholder(tf.float32, shape=(None,2), name='x')
-y = tf.placeholder(tf.float32, shape=(None,2), name='y')
+x = tf.placeholder(tf.float32, shape=(None, 2), name='x')
+y = tf.placeholder(tf.float32, shape=(None, 2), name='y')
 
 logist = tf.matmul(x, W) + b
 pred = tf.sigmoid(logist)
 # pred = tf.matmul(x, W) + b
 # 定义损失函数
 # loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logist, labels=y))
-loss = tf.reduce_mean(tf.square(y-pred))
+loss = tf.reduce_mean(tf.square(y - pred))
 optimizer = tf.train.GradientDescentOptimizer(step)
 train = optimizer.minimize(loss=loss)
 acc = []
+saver = tf.train.Saver()
+
 with tf.Session() as sess:
-    sess.run(tf.global_variables_initializer()) 
+    sess.run(tf.global_variables_initializer())
     for j in range(500):
-###################训练##############################
-        total_batch = int(len_train*2/batch_train)
+        avloss = 0.
+        ###################训练##############################
+        total_batch = int(len_train * 2 / batch_train)
         for i in range(int(total_batch)):
             #    print(i)
-            train_X = X[i*batch_train:(i+1)*batch_train, :]
-            train_Y = Y[i*batch_train:(i+1)*batch_train, :]
+            train_X = X[i * batch_train:(i + 1) * batch_train, :]
+            train_Y = Y[i * batch_train:(i + 1) * batch_train, :]
             #    print(train_Y)
             _, l = sess.run([train, loss], feed_dict={x: train_X, y: train_Y})
-###################测试##############################
-            if j % 100 == 0 and i==0:
-                print(j)
+            avloss += l / total_batch
+            ###################测试##############################
+            if j % 100 == 0 and i == 0:
+                saver.save(sess, './model/my_test_model')
+                print('\n*************第', j, '次迭代*****************', '\n')
+                print('损失: ', avloss, '\n')
                 rw = sess.run(W)
                 rb = sess.run(b)
                 print('训练结果:\n')
                 print('W=', rw, '\n\n', 'b=', rb, '\n')
                 acc.append(calc_accuracy(logist, y))
-                print('准确率：', acc[-1])
-###################画图##############################
-    N = 100
-    x1_min, x1_max =XA_test.min()-1, XA_test.max()+1   # x的范围
-    x2_min, x2_max = XB_test.min()-1, XB_test.max()+1   # y的范围
-    t1 = np.linspace(x1_min, x1_max, N)
-    t2 = np.linspace(x2_min, x2_max, N)
-    x1, x2 = np.meshgrid(t1, t2)                    # 生成网格采样点
-    x1.shape = (-1,1)
-    x2.shape = (-1,1)
-    X_feed = np.append(x1, x2, axis=1)
-    y_pred = sess.run(tf.argmax(logist, 1) , feed_dict={x: X_feed})
-    ###############重新给形状########################
-    x1, x2 = np.meshgrid(t1, t2)  
-    y_pred = y_pred.reshape(x1.shape)   
-    # print(y_pred)
-    plt.plot(Ax_test,Ay_test, 'k+')
-    plt.plot(Bx_test,By_test, 'bo')
-    cm_light = mpl.colors.ListedColormap(['#77E0A0', '#FF8080'])
-    plt.pcolormesh(x1, x2, y_pred, cmap=cm_light)     # 预测值的显示
-    plt.show()
-
-
-
-
-
-
-    
-
-    
-
-   
-
-
-   
-
-    
-   
-
+                print('准确率 ACC =', acc[-1])
+# ###################画图##############################
+#     N = 100
+#     x1_min, x1_max =XA_test.min()-1, XA_test.max()+1   # x的范围
+#     x2_min, x2_max = XB_test.min()-1, XB_test.max()+1   # y的范围
+#     t1 = np.linspace(x1_min, x1_max, N)
+#     t2 = np.linspace(x2_min, x2_max, N)
+#     x1, x2 = np.meshgrid(t1, t2)                    # 生成网格采样点
+#     x1.shape = (-1,1)
+#     x2.shape = (-1,1)
+#     X_feed = np.append(x1, x2, axis=1)
+#     y_pred = sess.run(tf.argmax(logist, 1) , feed_dict={x: X_feed})
+#     ###############重新给形状########################
+#     x1, x2 = np.meshgrid(t1, t2)
+#     y_pred = y_pred.reshape(x1.shape)
+#     # print(y_pred)
+#     plt.plot(Ax_test,Ay_test, 'k+')
+#     plt.plot(Bx_test,By_test, 'bo')
+#     cm_light = mpl.colors.ListedColormap(['#77E0A0', '#FF8080'])
+#     plt.pcolormesh(x1, x2, y_pred, cmap=cm_light)     # 预测值的显示
+#     plt.show()
---
+++ class2/class2_2_2.py
@@ -0,0 +1,85 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+
+
+def calc_accuracy(logist, y):
+    accuracy = tf.equal(tf.argmax(logist, 1), tf.argmax(y, 1))
+    accuracy = tf.cast(accuracy, tf.float32)
+    accuracy = tf.reduce_mean(accuracy)
+    return restore_sess.run(accuracy, feed_dict={x: X_test, y: Y_test})
+
+
+#######################恢复图##############################
+tf.reset_default_graph()
+restore_graph = tf.Graph()
+with restore_graph.as_default():
+    ##############基本设置########################
+    mu1x = 6
+    mu2x = 3
+    mu1y = 3
+    mu2y = 6
+    sigma = 1
+    ####测试####################################
+    #测试设置
+    batch_test = 30
+    len_test = 30
+    #测试数据
+    Ax_test = sigma * np.random.randn(len_test, 1) + mu1x
+    Ay_test = sigma * np.random.randn(len_test, 1) + mu1y
+    Bx_test = sigma * np.random.randn(len_test, 1) + mu2x
+    By_test = sigma * np.random.randn(len_test, 1) + mu2y
+
+    XA_test = np.append(Ax_test, Ay_test, axis=1)
+    XB_test = np.append(Bx_test, By_test, axis=1)
+    X_test = np.append(XA_test, XB_test, axis=0)
+
+    Y0_test = np.zeros(shape=(len_test, 1))
+    Y01_test = np.append(Y0_test, np.ones(shape=(len_test, 1)), axis=1)
+    Y1_test = np.ones(shape=(len_test, 1))
+    Y10_test = np.append(Y1_test, np.zeros(shape=(len_test, 1)), axis=1)
+    Y_test = np.append(Y10_test, Y01_test, axis=0)
+
+with tf.Session(graph=restore_graph) as restore_sess:
+    restore_saver = tf.train.import_meta_graph('./model/my_test_model.meta')
+    restore_saver.restore(restore_sess, tf.train.latest_checkpoint('./model'))
+    graph = tf.get_default_graph()
+    x = graph.get_tensor_by_name('x:0')
+    y = graph.get_tensor_by_name('y:0')
+    logist = graph.get_tensor_by_name('add:0')
+    acc = calc_accuracy(logist, y)
+    print('\n准确率 ACC =', acc)
+    ###################画图##############################
+    N = 100
+    x1_min, x1_max = XA_test.min() - 1, XA_test.max() + 1  # x的范围
+    x2_min, x2_max = XB_test.min() - 1, XB_test.max() + 1  # y的范围
+    t1 = np.linspace(x1_min, x1_max, N)
+    t2 = np.linspace(x2_min, x2_max, N)
+    x1, x2 = np.meshgrid(t1, t2)  # 生成网格采样点
+    x1.shape = (-1, 1)
+    x2.shape = (-1, 1)
+    X_feed = np.append(x1, x2, axis=1)
+    y_pred = restore_sess.run(tf.argmax(logist, 1), feed_dict={x: X_feed})
+    ###############重新给形状########################
+    x1, x2 = np.meshgrid(t1, t2)
+    y_pred = y_pred.reshape(x1.shape)
+    # print(y_pred)
+    plt.plot(Ax_test, Ay_test, 'k+')
+    plt.plot(Bx_test, By_test, 'bo')
+    cm_light = mpl.colors.ListedColormap(['#77E0A0', '#FF8080'])
+    plt.pcolormesh(x1, x2, y_pred, cmap=cm_light)  # 预测值的显示
+    plt.show()
+##################################另一种恢复#######################
+# sess =tf.Session()
+# saver = tf.train.import_meta_graph('./model/my_test_model.meta')
+# saver.restore(sess, tf.train.latest_checkpoint('./model'))
+# graph = tf.get_default_graph()
+# x = graph.get_tensor_by_name('x:0')
+# y = graph.get_tensor_by_name('y:0')
+# logist = graph.get_tensor_by_name('add:0')
+# accuracy = tf.equal(tf.argmax(logist, 1), tf.argmax(y, 1))
+# accuracy = tf.cast(accuracy, tf.float32)
+# accuracy = tf.reduce_mean(accuracy)
+# acc = sess.run(accuracy, feed_dict={x: X_test, y: Y_test})
+# print('准确率：', acc)
