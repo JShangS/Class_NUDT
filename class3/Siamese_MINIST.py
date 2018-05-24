d6a4ff9aa93edf54bfe5f0f23b8de0a357307694
JerryShang <424431687@qq.com>
2018-05-24 20:24:51 +0800

    commit

---
+++ class3/Siamese_MINIST.py
@@ -0,0 +1,76 @@
+﻿import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+import numpy as np
+import matplotlib.pyplot as plt
+
+tf.reset_default_graph()
+mnist = input_data.read_data_sets('./data/mnist',one_hot=False)
+print(mnist.validation.num_examples)
+print(mnist.train.num_examples)
+print(mnist.test.num_examples)
+
+def minist_draw(im):
+    im = im.reshape(28, 28)
+    fig = plt.figure()
+    plotwindow = fig.add_subplot(111)
+    plt.axis('off')
+    plt.imshow(im, cmap='gray')
+    plt.show()
+    # plt.savefig("test.png")  # ������ļ�
+    plt.close()
+
+def balanced_batch(batch_x,batch_y, num_cls):
+    batch_size=len(batch_y)
+    pos_per_cls_e=round(batch_size/2/num_cls/2)
+    pos_per_cls_e=2
+    index=batch_y.argsort()
+    ys_1=batch_y[index]
+    #print(ys_1)
+    
+    num_class=[]
+    pos_samples=[]
+    neg_samples=set()
+    cur_ind=0
+    for item in set(ys_1):
+        num_class.append((ys_1==item).sum())
+        num_pos=pos_per_cls_e
+        while(num_pos>num_class[-1]):
+            num_pos-=2
+        pos_samples.extend(np.random.choice(index[cur_ind:cur_ind+num_class[-1]],num_pos,replace=False).tolist())
+        neg_samples=neg_samples|(set(index[cur_ind:cur_ind+num_class[-1]])-set(list(pos_samples)))
+        cur_ind+=num_class[-1]
+    
+    neg_samples=list(neg_samples)
+    
+    x1_index=pos_samples[::2]
+    x2_index=pos_samples[1:len(pos_samples)+1:2]
+
+    x1_index.extend(neg_samples[::2])
+    x2_index.extend(neg_samples[1:len(neg_samples)+1:2])
+    
+    p_index=np.random.permutation(len(x1_index))
+    x1_index=np.array(x1_index)[p_index]
+    x2_index=np.array(x2_index)[p_index]
+
+    r_x1_batch=batch_x[x1_index]
+    r_x2_batch=batch_x[x2_index]
+    r_y_batch=np.array(batch_y[x1_index]!=batch_y[x2_index],dtype=np.float32)
+    return r_x1_batch,r_x2_batch,r_y_batch
+
+lr = 0.01
+iterations = 20000
+batch_size = 64
+
+with tf.Session() as sess:
+
+    sess.run(tf.global_variables_initializer())
+
+    for itera in range(iterations):
+
+        batch_x, batch_y = mnist.train.next_batch(batch_size*2)
+
+        xs_1,xs_2,y_s=balanced_batch(batch_x,batch_y, num_cls=10)
+
+        for i in range(10):
+            minist_draw(xs_1[i])
+            minist_draw(xs_2[i])
---
+++ class4/tensor_name.py
@@ -0,0 +1,115 @@
+﻿import tensorflow as tf
+import numpy as np
+
+with tf.name_scope('n_1'):
+    with tf.name_scope('n_2'):
+        with tf.variable_scope('v_1'):
+            with tf.variable_scope('v_2'):
+                Weights1 = tf.get_variable('Weights', shape=[2,3])
+                bias1 = tf.Variable([0.52], name='bias')
+
+
+print (Weights1.name)
+print (bias1.name)
+
+with tf.name_scope('n_1'):
+    with tf.name_scope('n_2'):
+        with tf.variable_scope('v_1'):
+            with tf.variable_scope('v_2'):
+                bias1 = tf.Variable([0.52], name='bias')
+
+print (bias1.name)
+
+with tf.variable_scope('v_scope') as scope1:
+    Weights1 = tf.get_variable('Weights', shape=[2,3])
+    bias1 = tf.Variable([0.52], name='bias')
+
+# 下面来共享上面已经定义好的变量
+# note: 在下面的 scope 中的get_variable()变量必须已经定义过了，才能设置 reuse=True，否则会报错
+with tf.variable_scope('v_scope', reuse=True) as scope2:
+    Weights2 = tf.get_variable('Weights')
+    bias2 = tf.Variable([0.52], name='bias')
+    bias3 = tf.Variable([0.52], name='bias')
+
+print (Weights1.name)
+print (Weights2.name)
+print (bias1.name)
+print (bias2.name)
+print (bias3.name)
+
+with tf.name_scope('conv1') as scope:
+    weights1 = tf.Variable([1.0, 2.0], name='weights')
+    bias1 = tf.Variable([0.3], name='bias')
+
+with tf.name_scope('conv2') as scope:
+    weights2 = tf.Variable([4.0, 2.0], name='weights')
+    bias2 = tf.Variable([0.33], name='bias')
+
+print (weights1.name)
+print (weights2.name)
+
+with tf.name_scope('conv1') as scope:
+    weights1 = tf.Variable([1.0, 2.0], name='weights')
+    bias1 = tf.Variable([0.3], name='bias')
+
+with tf.name_scope('conv2') as scope:
+    weights2 = tf.Variable([4.0, 2.0], name='weights')
+    bias2 = tf.Variable([0.33], name='bias')
+
+print (weights1.name)
+print (weights2.name)
+
+
+
+np.random.seed(1)
+b=tf.Variable(initial_value=np.random.randint(10),name='b')
+a=tf.Variable(initial_value=np.random.randint(10),name='a')
+c=tf.Variable(initial_value=np.random.randint(10),name='b')
+
+
+add0=tf.add(a,b)
+print(add0)
+add1=tf.add(a,b, name="add")
+print(add1)
+add2=tf.add(a,c, name="add")
+print(add2)
+
+t=tf.get_default_graph().get_tensor_by_name("add:0")
+print(t)
+
+# ��value�� is a tensor with shape [5, 30]
+# Split ��value�� into 3 tensors along dimension 1
+
+v_split=tf.Variable(initial_value=np.random.rand(5,30),name='v_split')
+all_v =tf.global_variables()  
+
+
+split0, split1, split2 = tf.split( v_split, 3,1, name='split_n')
+tf.shape(split0) 
+print(split0)
+print(split1)
+print(split2)
+#==> [5, 10]
+
+s_input=tf.constant([[[1, 1, 1], [2, 2, 2]],[[3, 3, 3], [4, 4, 4]],[[5, 5, 5], [6, 6, 6]]],name='s_input')
+#��input�� is 
+#[[[1, 1, 1], [2, 2, 2]],[[3, 3, 3], [4, 4, 4]],[[5, 5, 5], [6, 6, 6]]]
+s1=tf.slice(s_input, [1, 0, 0], [1, 1, 3]) #==> [[[3, 3, 3]]]
+print(s1)
+s2=tf.slice(s_input, [1, 0, 0], [1, 2, 3]) 
+print(s2)
+#==> 
+#[[[3, 3, 3],
+#[4, 4, 4]]]
+s3=tf.slice(s_input, [1, 0, 0], [2, 1, 3]) #==> 
+print(s3)
+#[[[3, 3, 3]],
+#[[5, 5, 5]]]
+
+for i in all_v:  
+    print  (i)
+
+tensor_name_list = [tensor.name for tensor in tf.get_default_graph().as_graph_def().node]
+for tensor_name in tensor_name_list:
+    print(tensor_name,'\n')
+
