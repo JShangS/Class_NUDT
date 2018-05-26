eba0526bed62f4d863d35c6b0799f7862dcd9dac
JerryShang <424431687@qq.com>
2018-05-26 10:32:23 +0800

    tensorboard_JS

---
+++ class5/class3_4_5.py
@@ -0,0 +1,188 @@
+# 该模型实现如下的功能，输入两个MINIST图片，判断是不是同一个数字。
+# 输入  负样本对：X1=6的图片 ， X2=9的图片 输出：1
+
+# 输入  正样本对：X1=3的图片 ， X2=3的图片 输出：0
+##用class3的程序来看看tensorboard，改了下class3的程序，使用变量复用
+import tensorflow as tf
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+LOGDIR = 'F:/PycharmProjects/Class_NUDT/class5/tensorboard/'
+
+
+##############平衡样本#############
+def balanced_batch(batch_x, batch_y, num_cls):
+    batch_size = len(batch_y)
+    pos_per_cls_e = round(batch_size / 2 / num_cls)
+    index = batch_y.argsort()
+    ys_1 = batch_y[index]
+    #print(ys_1)
+
+    num_class = []
+    pos_samples = []
+    neg_samples = set()
+    cur_ind = 0
+    for item in set(ys_1):
+        num_class.append((ys_1 == item).sum())
+        num_pos = pos_per_cls_e
+        while (num_pos > num_class[-1]):
+            num_pos -= 2
+        pos_samples.extend(
+            np.random.choice(
+                index[cur_ind:cur_ind + num_class[-1]], num_pos,
+                replace=False).tolist())
+        neg_samples = neg_samples | (set(
+            index[cur_ind:cur_ind + num_class[-1]]) - set(list(pos_samples)))
+        cur_ind += num_class[-1]
+
+    neg_samples = list(neg_samples)
+
+    x1_index = pos_samples[::2]
+    x2_index = pos_samples[1:len(pos_samples) + 1:2]
+
+    x1_index.extend(neg_samples[::2])
+    x2_index.extend(neg_samples[1:len(neg_samples) + 1:2])
+
+    p_index = np.random.permutation(len(x1_index))
+    x1_index = np.array(x1_index)[p_index]
+    x2_index = np.array(x2_index)[p_index]
+
+    r_x1_batch = batch_x[x1_index]
+    r_x2_batch = batch_x[x2_index]
+    r_y_batch = np.array(
+        batch_y[x1_index] != batch_y[x2_index], dtype=np.float)
+    r_y_batch = np.expand_dims(r_y_batch, 0)
+    return r_x1_batch, r_x2_batch, r_y_batch
+
+
+###########计算准确率################################
+def calc_accuracy(pred, y):
+    accuracy = tf.equal(tf.to_float(pred > 2.5), y)
+    # accuracy = tf.equal(pred, y)
+    accuracy = tf.cast(accuracy, tf.float32)
+    accuracy = tf.reduce_mean(accuracy, name='Accuracy')
+    tf.summary.histogram('accuracy', accuracy)
+    return sess.run(accuracy, feed_dict={x1: xs_t1, x2: xs_t2, y: y_ts})
+
+
+##################添加网络的函数2个输入2输出###########################
+def add_layer(name,
+              inputs1,
+              inputs2,
+              in_size,
+              out_size,
+              activation_function=None):
+    with tf.name_scope(name):
+        ###########强行变量复用######################
+        name_var = name + 'var'
+        with tf.variable_scope(name_var):
+            Weights = tf.get_variable(
+                'w', initializer=tf.random_normal(
+                    [in_size, out_size]))  #random_normal#zeros
+            biases = tf.get_variable(
+                'b', initializer=tf.random_normal([1, out_size]))
+            Wx_plus_b1 = tf.matmul(inputs1, Weights) + biases
+        with tf.variable_scope(name_var, reuse=True):
+            Weights = tf.get_variable('w')
+            biases = tf.get_variable('b')
+            Wx_plus_b2 = tf.matmul(inputs2, Weights) + biases
+        if activation_function is None:
+            outputs1 = Wx_plus_b1
+            outputs2 = Wx_plus_b2
+        else:
+            outputs1 = activation_function(Wx_plus_b1)
+            outputs2 = activation_function(Wx_plus_b2)
+        #####################################
+        tf.summary.histogram("weights", Weights)
+        tf.summary.histogram("biases", biases)
+        tf.summary.histogram("activations1", outputs1)
+        tf.summary.histogram("activations2", outputs2)
+        #####################################
+        return outputs1, outputs2
+
+
+##########读取数据################################
+old_v = tf.logging.get_verbosity()
+tf.logging.set_verbosity(tf.logging.ERROR)
+mnist = input_data.read_data_sets('./data/mnist', one_hot=False)
+# train_data = mnist.train.images  # Returns np.array
+# train_labels = np.asarray(mnist.train.labels, dtype=np.float)
+# test_data = mnist.test.images
+# test_labels = np.asarray(mnist.train.labels, dtype=np.float)
+
+#####################构图#####################
+batch_size = 64
+x1 = tf.placeholder(tf.float32, shape=[None, 784], name='input1')
+x2 = tf.placeholder(tf.float32, shape=[None, 784], name='input2')
+y = tf.placeholder(tf.float32, shape=[1, None], name='label')
+x_image1 = tf.reshape(x1, [-1, 28, 28, 1])
+x_image2 = tf.reshape(x2, [-1, 28, 28, 1])
+tf.summary.image('input1', x_image1)
+tf.summary.image('input2', x_image2)
+
+###第一层###################
+L11, L12 = add_layer('fc1', x1, x2, 784, 500, tf.nn.leaky_relu)  #tf.nn.sigmoid
+###第二个层###################
+L21, L22 = add_layer(
+    'fc2', L11, L12, 500, 10,
+    tf.nn.leaky_relu)  #tf.nn.softmax #tf.nn.relu#tf.nn.leaky_relu
+###############计算Ew和预测##################
+Ew = tf.sqrt(tf.reduce_sum(tf.square(tf.subtract(L21, L22)), 1), name='Ew')
+tf.summary.histogram('Ew', Ew)
+pred = Ew  #tf.nn.sigmoid(Ew)
+# pred = tf.to_float(tf.equal(tf.argmax(L21, 1), tf.argmax(L22, 1)))
+############loss#############
+Q = tf.constant(5, dtype=tf.float32)
+# loss = tf.reduce_mean(tf.square(y - pred))
+loss = tf.add(
+    tf.multiply(
+        tf.multiply(tf.subtract(1.0, y), tf.div(2.0, Q)), tf.square(Ew)),
+    tf.multiply(
+        tf.multiply(tf.multiply(2.0, y), Q),
+        tf.exp(tf.multiply(tf.div(-2.77, Q), Ew))))
+tf.summary.histogram('loss', loss)
+lr = 1e-2
+optimizer = tf.train.AdamOptimizer(
+    learning_rate=lr)  #AdamOptimizer#GradientDescentOptimizer
+train = optimizer.minimize(loss=(loss))  #tf.reduce_mean
+################会话############################
+ACC = []
+merge_all = tf.summary.merge_all()
+writer = tf.summary.FileWriter(LOGDIR + 'JS_board1S/')
+# saver = tf.train.Saver()
+with tf.Session() as sess:
+    iter = 20001
+    #########
+    writer.add_graph(sess.graph)
+    ############
+    sess.run(tf.global_variables_initializer())
+    for itera in range(iter):
+        batch_x, batch_y = mnist.train.next_batch(batch_size * 2)
+        xs_1, xs_2, y_s = balanced_batch(batch_x, batch_y, num_cls=10)
+        ##############每隔50次写入一次####################
+        if itera % 50 == 0:
+            s = sess.run(merge_all, feed_dict={x1: xs_1, x2: xs_2, y: y_s})
+            writer.add_summary(s, itera)
+            writer.flush()
+        _, l, y_pred, rEw = sess.run(
+            [train, loss, pred, Ew], feed_dict={
+                x1: xs_1,
+                x2: xs_2,
+                y: y_s
+            })
+
+        if itera % 1000 == 0:
+            print('\n')
+            print('第', itera, '次迭代，损失为： ', sess.run(tf.reduce_mean(l)))
+            # print(y_s.reshape((1, -1)))
+            # print('Ew', rEw, '\n')
+            #######################测试#############################
+            batch_x, batch_y = mnist.test.next_batch(batch_size * 2)
+            xs_t1, xs_t2, y_ts = balanced_batch(batch_x, batch_y, num_cls=10)
+            #######准确率########################
+            # ACC.append(calc_accuracy(pred, y))
+            ACC = calc_accuracy(pred, y)
+            print('准确率 ACC =', ACC)
+            # saver.save(sess, './model_class3/my_test_model')
+    writer.close()
+tf.logging.set_verbosity(old_v)
--- class5/lenet_part_sum.py
+++ class5/lenet_part_sum.py
@@ -2,34 +2,45 @@
 # Author: Dengwen Lin
 # Date: 2018/05/18
 
-import numpy as np 
+import numpy as np
 import tensorflow as tf
-from tensorflow.contrib.layers import flatten 
+from tensorflow.contrib.layers import flatten
 
 
-    def _cnn_layer(self, scope_name, W_name, b_name, x, filter_shape, conv_strides, padding_tag='VALID'):
-        with tf.name_scope(scope_name):
-            conv_W = tf.get_variable(W_name, dtype=tf.float32, 
-            	initializer=tf.truncated_normal(shape=filter_shape, mean=self.config.mu, stddev=self.config.sigma))
+def _cnn_layer(self,
+               scope_name,
+               W_name,
+               b_name,
+               x,
+               filter_shape,
+               conv_strides,
+               padding_tag='VALID'):
+    with tf.name_scope(scope_name):
+        conv_W = tf.get_variable(
+            W_name,
+            dtype=tf.float32,
+            initializer=tf.truncated_normal(
+                shape=filter_shape,
+                mean=self.config.mu,
+                stddev=self.config.sigma))
 
-            conv_b = tf.get_variable(b_name, dtype=tf.float32, 
-            	initializer=tf.zeros(filter_shape[3]))
-            conv   = tf.nn.conv2d(x, conv_W, strides=conv_strides, padding=padding_tag) + conv_b
-            
-            with tf.name_scope('visual') as v_s:
-                # scale weights to [0 1], type is still float
-                x_min = tf.reduce_min(conv_W)
-                x_max = tf.reduce_max(conv_W)
-                kernel_0_to_1 = (conv_W - x_min) / (x_max - x_min)
-                # to tf.image_summary format [batch_size, height, width, channels]
-                kernel_transposed = tf.transpose (kernel_0_to_1, [3, 0, 1, 2])
-                # this will display random 3 filters from the 64 in conv1
-                show_img=tf.cond(tf.greater( filter_shape[2],1), lambda: tf.slice(kernel_transposed,[0,0,0,0],[-1,filter_shape[0],filter_shape[1],3]),lambda: kernel_transposed)
-                tf.summary.image('conv_w', show_img, max_outputs=filter_shape[3])
-                layer1_image1 = conv[0:1, :, :, 0:filter_shape[3]]
-                layer1_image1 = tf.transpose(layer1_image1, perm=[3,1,2,0])
-                tf.summary.image('feature', layer1_image1, max_outputs=16)
-            
-            return conv
-    
- 
+        conv_b = tf.get_variable(
+            b_name, dtype=tf.float32, initializer=tf.zeros(filter_shape[3]))
+        conv = tf.nn.conv2d(
+            x, conv_W, strides=conv_strides, padding=padding_tag) + conv_b
+
+    with tf.name_scope('visual') as v_s:
+        # scale weights to [0 1], type is still float
+        x_min = tf.reduce_min(conv_W)
+        x_max = tf.reduce_max(conv_W)
+        kernel_0_to_1 = (conv_W - x_min) / (x_max - x_min)
+        # to tf.image_summary format [batch_size, height, width, channels]
+        kernel_transposed = tf.transpose(kernel_0_to_1, [3, 0, 1, 2])
+        # this will display random 3 filters from the 64 in conv1
+        show_img=tf.cond(tf.greater( filter_shape[2],1), lambda: tf.slice(kernel_transposed,[0,0,0,0],[-1,filter_shape[0],filter_shape[1],3]),lambda: kernel_transposed)
+        tf.summary.image('conv_w', show_img, max_outputs=filter_shape[3])
+        layer1_image1 = conv[0:1, :, :, 0:filter_shape[3]]
+        layer1_image1 = tf.transpose(layer1_image1, perm=[3, 1, 2, 0])
+        tf.summary.image('feature', layer1_image1, max_outputs=16)
+
+        return conv
